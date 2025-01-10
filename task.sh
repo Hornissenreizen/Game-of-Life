2964a6e47703e3ed53a833cbf9178f288ac768cf
Stefan Wagner <112750321+Stdehnbostel@users.noreply.github.com>
2025-01-10 12:36:07 +0100

    adjust slum script to work with mpi

--- task.sh
+++ task.sh
@@ -4,10 +4,15 @@
 #SBATCH --error=project.err
 #SBATCH --partition=short
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=4
 #SBATCH --time=0:30:00
 #SBATCH --cpus-per-task=1
 #SBATCH --exclusive
 
+# compile with version 4.1.1
 module load mpi/openmpi/4.1.1
-make
+mpicxx main.cpp -o game_of_life
+
+# change the MPI version, because Draco is not set up correctly.
+module load mpi/openmpi/4.1.0
+mpirun -np 4 game_of_life
